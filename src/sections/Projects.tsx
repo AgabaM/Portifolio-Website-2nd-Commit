531288c481b7c2f41b94be21838cb21f51fb4503
AGABA MOSES <agabamoses811@gmail.com>
2025-04-08 01:55:22 +0300

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -76,7 +76,7 @@ export const Projects = () => {
             <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
             <ul className="flex flex-col gap-4 mt-4 md:mt-5">
               {project.results.map(result =>(
-                <li className="flex gap-2 text-sm md:text-base text-white/50">
+                <li key={result.title} className="flex gap-2 text-sm md:text-base text-white/50">
                   <CheckCircleIcon className="size-5 md:size-6"/>
                   <span>{result.title}</span>
                   </li>
@@ -101,5 +101,5 @@ export const Projects = () => {
       </div>
       </div>
   
-  </section>;
+  </section>
 };
